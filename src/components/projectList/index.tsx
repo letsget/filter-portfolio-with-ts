ac4c4e5573e05c172d323a569794443c6384e70c
letsget <andrewbond25@gmail.com>
2020-11-14 15:32:10 +0200

    fix: added distructuring

--- src/components/projectList/index.tsx
+++ src/components/projectList/index.tsx
@@ -7,9 +7,9 @@ interface Props {
 
 const ProjectList: FC<Props> = ({ projects }) => (
   <div className="projects">
-    {projects.map((project: ProjectType) => (
-      <div key={project.id}>
-        <img className="image" src={project.img} alt="portfolio item" />
+    {projects.map(({ id, img }: ProjectType) => (
+      <div key={id}>
+        <img className="image" src={img} alt="portfolio item" />
       </div>
     ))}
   </div>
